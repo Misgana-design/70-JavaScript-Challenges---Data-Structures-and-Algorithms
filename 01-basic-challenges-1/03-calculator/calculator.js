e45a77c3cab4c58188e11235a739b095f6d18ca9
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-26 05:59:17 -0700

    01-Basic Challenges-03-calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(a, b, o) {
+  let result;
+  switch (o) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    default:
+      throw new Error("It is invalid");
+  }
+  return result;
+}
 
 module.exports = calculator;
