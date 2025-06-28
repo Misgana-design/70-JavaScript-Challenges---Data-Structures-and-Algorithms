7ba3a375d51a5cddbe84eb4163361c53fc86a4be
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-28 07:31:03 -0700

    01-Basic Challenges-04-Calculaotor

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,18 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let c = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (char === str[i]) {
+      c = c + 1;
+    }
+  }
+  return c;
+  // let words = str.split("");
+  // for (i = 0; i < str.length; i++) {
+  //   if (words[i] === char) {
+  //     c++;
+  //   }
+  // }
+  // return c++;
+}
 
 module.exports = countOccurrences;
