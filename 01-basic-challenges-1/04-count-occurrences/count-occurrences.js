11ea5c38fb2dcace0611bd94e256b81eaa9b0b84
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-30 13:23:40 -0700

    Changed

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,25 +1,26 @@
 //Solution 1
-function countOccurrences(str, char) {
-  let c = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (char === str[i]) {
-      c = c + 1;
-    }
-  }
-  return c;
-}
+// function countOccurrences(str, char) {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (char === str[i]) {
+//       count = count + 1;
+//     }
+//   }
+//   return count;
+// }
 
 // Solution 2
-function countOccurrences(str, char) {
-  let words = str.split("");
-  for (i = 0; i < str.length; i++) {
-    if (words[i] === char) {
-      c++;
-    }
-  }
+// function countOccurrences(str, char) {
+//   let count = 0;
+//   let words = str.split("");
+//   for (i = 0; i < str.length; i++) {
+//     if (words[i] === char) {
+//       count++;
+//     }
+//   }
 
-  return c++;
-}
+//   return count++;
+// }
 
 //Solution 3
 function countOccurrences(str, char) {
