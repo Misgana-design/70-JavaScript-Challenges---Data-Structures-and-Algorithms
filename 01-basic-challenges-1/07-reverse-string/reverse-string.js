60dd8179e6649b29eb3cf4a348d6e2ba12bef1fa
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-30 19:52:55 -0700

    01-Basic Challenges-07-reverse-string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,16 @@
-function reverseString() {}
+//Solution 1
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+  return reversed;
+}
+
+//Solution 2
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
 
 module.exports = reverseString;
