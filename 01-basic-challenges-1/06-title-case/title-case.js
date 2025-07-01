409b8633beea3636746b6b74253e7ce61fa84acf
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-30 19:08:18 -0700

    01-Basic Challenges-06-title-case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,18 @@
-function titleCase() {}
+// function titleCase(string) {
+//   let words = string.toLowerCase().split(" ");
+//   for (i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//   }
+//   return words.join(" ");
+// }
+
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => (word ? word[0].toUpperCase() + word.slice(1) : ""))
+    .join(" ");
+}
+  
 
 module.exports = titleCase;
