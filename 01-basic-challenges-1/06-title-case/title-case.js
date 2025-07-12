5ef767004da89b8604fe49c6c3f7d4b8c5083bd1
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-12 06:11:03 -0700

    01-basic-challenges-1/06-title-case/title-case.js

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,10 +1,15 @@
-// function titleCase(string) {
-//   let words = string.toLowerCase().split(" ");
-//   for (i = 0; i < words.length; i++) {
-//     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-//   }
-//   return words.join(" ");
-// }
+// Solution 1
+
+function titleCase(string) {
+  let words = string.toLowerCase().split(" ");
+  for (i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    console.log(words[i]);
+  }
+  return words.join(" ");
+}
+
+//Solution 2
 
 function titleCase(str) {
   return str
@@ -13,6 +18,24 @@ function titleCase(str) {
     .map((word) => (word ? word[0].toUpperCase() + word.slice(1) : ""))
     .join(" ");
 }
-  
+
+//Solution 3
+
+// function titleCase(str) {
+//   const words = str.toLowerCase().split(" ");
+//   let wor = "";
+//   for (let i = 0; i < words.length; i++) {
+//     wor = wor + (words[i][0].toUpperCase() + words[i].slice(1)) + " ";
+//   }
+//   return wor.trim();
+// }
+
+// function titleCase(string) {
+//   return string
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) => (true ? word[0].toUpperCase() + word.slice(1) : ""))
+//     .join(" ");
+// }
 
 module.exports = titleCase;
