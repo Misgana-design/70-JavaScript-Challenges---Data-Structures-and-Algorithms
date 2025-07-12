f74347ab90eab85c6772e9483be7dc98b96ac9e4
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-12 02:31:09 -0700

    01-basic-challenges-1/08-palindrome/palindrome.js

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,26 @@
-function isPalindrome() {}
+//Solution 1
+
+function isPalindrome(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Solution 2
+
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;
