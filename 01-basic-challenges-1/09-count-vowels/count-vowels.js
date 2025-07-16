e3bd80d893b5a54f9f762b45be5b14b5b10fd72f
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-16 03:28:41 -0700

    01-basic-challenges-1/09-count-vowels/count-vowels.js

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(str) {
+  let formatedStr = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < formatedStr.length; i++) {
+    const char = formatedStr[i];
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
