2ff651eb2a690242724fd143abdb26335a9116fb
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-17 06:07:46 -0700

    01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,11 +1,21 @@
+//Solution 1
+
+// function removeDuplicates(arr) {
+//   const newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!newArray.includes(arr[i])) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
+// }
+
+
+
+//Solution 2 with Javascript built-in function called Set object
+
 function removeDuplicates(arr) {
-  const newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!newArray.includes(arr[i])) {
-      newArray.push(arr[i]);
-    }
-  }
-  return newArray;
+  return Array.from(new Set(arr));
 }
 
 module.exports = removeDuplicates;
