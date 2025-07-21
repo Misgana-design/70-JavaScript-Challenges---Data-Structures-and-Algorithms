95f3d162fd411ae0de9a8286fb9aeec7079c6b3a
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-21 05:36:08 -0700

    02-basic-challenges-2/02-array-intersection/array-intersection.js

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,26 @@
-function arrayIntersection() {}
+//Solution 1
+
+// function arrayIntersection(arr1, arr2) {
+//   let newArr = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !newArr.includes(arr1[i])) {
+//       newArr.push(arr1[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+//Solution 2
+
+function arrayIntersection(arr1, arr2) {
+  let set1 = new Set(arr1);
+  let newArr = [];
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      newArr.push(num);
+    }
+  }
+  return newArr;
+}
 
 module.exports = arrayIntersection;
