9cc1476ff2275f7f32915be0f38a9da4e53ecced
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-21 11:48:18 -0700

    02-basic-challenges-2/04-find-missing-number/find-missing-number.js

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,11 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+  let expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum = actualSum + arr[i];
+  }
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
