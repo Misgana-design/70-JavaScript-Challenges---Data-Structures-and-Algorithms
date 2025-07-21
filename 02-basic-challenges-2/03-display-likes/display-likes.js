7a61235f1e12fc325c90050ac6f1396617342806
Misgana-design <misganawoldeselassie@gmail.com>
2025-07-21 10:51:26 -0700

    02-basic-challenges-2/03-display-likes/display-likes.test.js

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,23 @@
-function displayLikes() {}
+function displayLikes(names) {
+  let length = names.length;
+
+  if (length === 0) {
+    return "no one likes this";
+  } else if (length === 1) {
+    return `${names} likes this`;
+  } else if (length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${length - 2} others like this`;
+  }
+
+  // "Peter likes this";
+  // "Jacob and Alex like this";
+  // "Max, John and Mark like this";
+  // "Alex, Jacob and 2 others like this";
+  // "Alex, Jacob and 3 others like this";
+}
 
 module.exports = displayLikes;
