591c9d500af3c9e8323ceca2652e891dfa02ee89
Misgana-design <misganawoldeselassie@gmail.com>
2025-08-06 12:37:03 +0300

    02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,28 @@
-function areAllCharactersUnique() {}
+//Solution 1
+function areAllCharactersUnique(str) {
+  let charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charCount[char]) {
+      return false;
+    }
+    charCount[char] = true;
+  }
+  return true;
+}
+
+//Solution 2
+
+function areAllCharactersUnique(str) {
+  let charSet = new Set();
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charSet.has(char)) {
+      return false;
+    }
+    charSet.add(char);
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
