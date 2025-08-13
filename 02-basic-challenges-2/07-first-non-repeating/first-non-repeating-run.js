3871380bec4bc174ce4924ff2c0b47afed2d39fe
Misgana-design <misganawoldeselassie@gmail.com>
2025-08-13 18:04:05 +0300

    02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
@@ -3,5 +3,5 @@ const findFirstNonRepeatingCharacter = require('./first-non-repeating');
 const result1 = findFirstNonRepeatingCharacter('programming');
 const result2 = findFirstNonRepeatingCharacter('abacddbec');
 
-console.log(result1);
+// console.log(result1);
 console.log(result2);
