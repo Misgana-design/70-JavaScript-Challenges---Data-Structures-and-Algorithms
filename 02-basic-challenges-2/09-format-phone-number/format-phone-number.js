21844874f2e9c6f93fcc579b9654b69af77afc59
Misgana-design <misganawoldeselassie@gmail.com>
2025-08-14 13:53:14 +0300

    02-basic-challenges-2/09-format-phone-number/format-phone-number.js

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,24 @@
-function formatPhoneNumber() {}
+//Solution
+// function formatPhoneNumber(arrNum) {
+//   const firstNum = arrNum.slice(0, 3).join("");
+//   const secondNum = arrNum.slice(3, -4).join("");
+//   const lastNum = arrNum.slice(6).join("");
+//   return `(${firstNum}) ${secondNum}-${lastNum}`;
+// }
+
+//Solution 2
+// function formatPhoneNumber(arrNum) {
+//   let formatedNumber = arrNum.join("");
+//   return `(${formatedNumber.substring(0, 3)}) ${formatedNumber.substring(
+//     3,
+//     -4
+//   )}-${formatedNumber.substring(6)}`;
+// }
+
+//Solution 3
+let formatPhoneNumber = (arrNum) =>
+  `(${arrNum.slice(0, 3).join("")}) ${arrNum.slice(3, -4).join("")}-${arrNum
+    .slice(6)
+    .join("")}`;
 
 module.exports = formatPhoneNumber;
