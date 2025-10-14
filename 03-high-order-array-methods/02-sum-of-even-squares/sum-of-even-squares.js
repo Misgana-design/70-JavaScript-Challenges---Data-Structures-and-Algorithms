462497fe573f9e90ecd1bee223397194fe46be4a
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-14 16:48:12 +0300

    03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,10 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const evenNum = arr.filter((num) => num % 2 === 0);
+  const evenSquare = evenNum.map((num) => num * num);
+  const evenSquareSum = evenSquare.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return evenSquareSum;
+}
 
 module.exports = sumOfEvenSquares;
