e8b05269033191616d6df7979a1cfbb7c7edef1d
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-18 16:03:01 +0300

    03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const firstCount = str1.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+  const secondCount = str2.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+  
+  const value = Object.keys(firstCount).every((char) => {
+    return firstCount[char] === secondCount[char];
+  });
+  return value;
+}
 
 module.exports = validAnagrams;
