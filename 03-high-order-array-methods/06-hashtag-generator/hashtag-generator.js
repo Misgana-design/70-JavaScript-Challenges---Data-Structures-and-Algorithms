85d99e1a19b84e89185ff97ca1aaf5c534ed32a9
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-20 15:55:40 +0300

    03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,27 @@
-function generateHashtag() {}
+//Solution 1
+
+// function generateHashtag(str) {
+//   if (str.trim() === "") return false;
+//   const words = str
+//     .trim()
+//     .split(/\s+/)
+//     .map((word) => word[0].toUpperCase() + word.slice(1));
+//   const hashTag = "#" + words.join("");
+//   return hashTag.length > 140 ? false : hashTag;
+// }
+
+//Solution 2
+
+function generateHashtag(str) {
+  if (str.trim() === "") return false;
+  const hashTag = str
+    .trim()
+    .split(/\s+/)
+    .reduce((acc, word) => {
+      return acc + word[0].toUpperCase() + word.slice(1);
+    }, "#");
+
+  return hashTag.length > 140 ? false : hashTag;
+}
 
 module.exports = generateHashtag;
