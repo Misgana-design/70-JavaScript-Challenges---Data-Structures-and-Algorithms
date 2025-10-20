15249f047e925c4737c522da50a49a7d111219c0
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-20 16:24:42 +0300

    03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,11 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+  const octets = ip.split(".");
+  if (!(octets.length === 4)) return false;
+
+  return octets.every((octet) => {
+    const num = parseInt(octet);
+    return num >= 0 && num <= 255 && octet === num.toString();
+  });
+};
 
 module.exports = isValidIPv4;
