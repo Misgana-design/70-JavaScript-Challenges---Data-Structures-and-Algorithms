e7085a859a98ab5bb22f9ef247c1ac503187814e
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-24 22:39:58 +0300

    03-high-order-array-methods/09-password-validator/password-validator.js

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,14 @@
-function validatePassword() {}
+function validatePassword(pass) {
+  const isLengthValid = pass.length >= 8;
+  const isUpperCase = pass
+    .split("")
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+  const isLowerCase = pass
+    .split("")
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+  const isDigit = pass.split("").some((num) => !isNaN(parseInt(num)));
+
+  return isLengthValid && isUpperCase && isLowerCase && isDigit;
+}
 
 module.exports = validatePassword;
