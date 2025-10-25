94c35db7c4d7120906785a201dcbb97506d259ae
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-25 14:52:49 +0300

    03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,35 @@
-function findMissingLetter() {}
+//Solution 1
+
+// function findMissingLetter(arr) {
+//   let start = arr[0].charCodeAt(0);
+
+//   const missingChar = arr
+//     .map((char) => char.charCodeAt())
+//     .find((current) => {
+//       if (current - start > 1) {
+//         return true;
+//       }
+//       start = current;
+//       return false;
+//     });
+//   return missingChar ? String.fromCharCode(start + 1) : "";
+// }
+
+//Solution 2
+
+function findMissingLetter(arr) {
+  let start = arr[0].charCodeAt(0);
+
+  const missingChar = arr
+    .map((char) => char.charCodeAt())
+    .find((current) => {
+      if (current - start > 1) {
+        return true;
+      }
+      start = current;
+      return false;
+    });
+  return missingChar ? String.fromCharCode(missingChar - 1) : "";
+}
 
 module.exports = findMissingLetter;
